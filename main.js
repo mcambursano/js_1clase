69b9be1c92b70208ec9829a9438a046a6c2fe58f
Martin Cambursano <tincambursano@outlook.com>
2023-03-10 18:50:31 -0300

    Primer Ejecicio

---
+++ main.js
@@ -0,0 +1,17 @@
+/* Primer Ejecicio */
+
+let name = prompt ("Escribe tu nombre y apellido")
+alert ("Hola " + name)
+console.log (name)
+alert ("Vamos a sumar, Te voy a pedir que coloques dos numeros")
+let numeroA = prompt ("Escribe el primer numero")
+let numeroB = prompt ("Escribe el siguiente numero")
+numeroA = parseFloat(numeroA)
+numeroB = parseFloat(numeroB)
+console.log (numeroA)
+console.log (numeroB)
+let suma = numeroA + numeroB
+console.log (suma)
+alert ("El Resultado es " + suma)
+
+
